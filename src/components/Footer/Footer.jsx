38a847ccfca29dcb5060c91e4ce864d5aa4c7377
niki2422 <randazzonico55@gmail.com>
2026-03-17 09:59:08 -0300

    se creo el Footer  con su respectivo css

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,25 @@
+import "./Footer.css"
+
+export default function Footer(){
+
+return(
+
+<footer className="footer">
+
+<p>
+React-Celu - Bahía Blanca
+</p>
+
+<p>
+Av. Siempre Viva 742
+</p>
+
+<p>
+Tel: 291-555-1234
+</p>
+
+</footer>
+
+)
+
+}
